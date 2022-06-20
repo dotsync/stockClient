bcc755dca3aa3526d739cfcb66e458ae37bf24a7
metablockdev <liddell.s@pm.me>
2022-06-20 16:45:07 -0700

    Implements Drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
-import { gql, useQuery } from '@apollo/client'
-import React from 'react'
 import './App.css'
-import TickerDetailsPage from './pages/TickerDetailsPage'
-import { Routes, Route, useParams, BrowserRouter } from 'react-router-dom'
-import TickerChart from './components/ticker/TickerChart'
-import Drawer from './components/shared/Drawer'
-// import Ticker from './components/ticker/Ticker'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import CustomDrawer from './components/shared/CustomDrawer'
 
 function App() {
+  console.log('sdgsfdhg')
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/details" element={<TickerProfilePage />}/>
-        <Route path=":ticker" element={<Dashboard />} /> */}
         <Route path="ticker" />
-        <Route path=":ticker" element={<Drawer />} />
+        <Route path=":ticker" element={<CustomDrawer />} />
       </Routes>
     </BrowserRouter>
   )
